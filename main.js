8a4c2cb02baa30f840c25cfc54b390f898423601
Ming He <meticulous.dft@gmail.com>
2014-09-16 00:42:35 -0700

    chapter 2 exercise 1 -- Controller

---
+++ main.js
@@ -0,0 +1,14 @@
+function MainCtrl($scope){
+
+  $scope.data = { greeting: "Hello" };
+
+  //change the character
+  $scope.data.character='unknown';
+
+  //language and acronym are missing, please help write them
+
+  //language
+
+  //acronym
+
+}

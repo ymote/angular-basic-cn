8e29f62759252c1b75aacd4486b1edab5d7f9f07
Ming He <meticulous.dft@gmail.com>
2014-09-17 00:58:46 -0700

    chapter 2 exercise 2 methods in controllers

--- main.js
+++ main.js
@@ -2,26 +2,47 @@ var myApp = angular.module('myApp', []);
 
 myApp.factory('DataStore', function() {
   var dataToShare = {
-    message: "Use service to share data between controllers", 
-    //todo
-    todo: 'Learn AngularJS'
+    message: "rats live on no evil star", 
   };
 
-  return dataToShare;
+  var setMessage = function(message){
+    dataToShare.message = message;
+  };
+
+  var getMessage = function(){
+    return dataToShare.message;
+  }
+
+  return {
+    'getMessage': getMessage,
+    'setMessage': setMessage,
+  } 
 })
 
 function FirstCtrl($scope, DataStore){
 
-  $scope.message = DataStore.message;
+  $scope.message = DataStore.getMessage();
 
-  //add todo variable
+  $scope.saveMessage = function(){
+    //调用工厂中setMessage方法来保存更新的message
+    
+  };
 
 }
 
 function SecondCtrl($scope, DataStore){
 
-  $scope.message = DataStore.message;
+  $scope.message = DataStore.getMessage();
 
-  //add todo variable
+  $scope.reversedMessage = function(message) {
+    return message.split("").reverse().join("");
+  };
+
+  $scope.fetchMessage = function(){
+    //调用工厂中getMessage方法来获取更新过的message
+
+    //调用控制域上reversedMessage方法来更新倒序显示的消息
+
+  };
 
 }

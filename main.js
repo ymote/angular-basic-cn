0c196336a1728c81aa40ad7d738ba6d2d5842f3b
Ming He <hmchem@gmail.com>
2014-09-17 11:45:11 -0700

    chapter 3 exercise 1 create filter

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 var myApp = angular.module('myApp', []);
 
+myApp.filter('myReverse',function(){
+  //input是过滤器作用的对象，uppercase是传入的参数
+  //比如{{ message | myReverse:true }}，input是message,uppercase是true
+  return function(input, uppercase){
+    var out = "";
+    for (var i = 0; i < input.length; i++) {
+      out = input.charAt(i) + out;
+    }
+    // conditional based on optional argument
+    if (uppercase) {
+      out = out.toUpperCase();
+    }
+    return out;    
+  }
+})
+
+//给定字符串并传入要移动的位数，过滤器将字符串每一位右移指定的位数，最末位补到最前。
+//比如{{ 'abcd' | shiftString:2 }} -> 'cdab'
+myApp.filter('shiftString',function(){
+  return function(input, numberToShift){
+    //首先取numberToShift对input.length的余数
+    numberToShift = numberToShift%input.length;
+    //使用数组储存新的字符串中的字符
+    var letters = new Array(input.length);
+    for (var i = 0; i < input.length; i++) {
+      //依照规则遍历字符串并存在letters的相对应的位置
+
+
+
+    }
+    //将改造好的数组转换成字符串输出  
+    return letters.join('');    
+  }  
+})
+
+function MainCtrl($scope, DataStore){
+
+  $scope.message = DataStore.getMessage();
+
+  $scope.reversedMessage = function(message) {
+    return message.split("").reverse().join("");
+  };
+
+}
+
 myApp.factory('DataStore', function() {
   var dataToShare = {
     message: "rats live on no evil star", 
@@ -18,31 +63,3 @@ myApp.factory('DataStore', function() {
     'setMessage': setMessage,
   } 
 })
-
-function FirstCtrl($scope, DataStore){
-
-  $scope.message = DataStore.getMessage();
-
-  $scope.saveMessage = function(){
-    //调用工厂中setMessage方法来保存更新的message
-    
-  };
-
-}
-
-function SecondCtrl($scope, DataStore){
-
-  $scope.message = DataStore.getMessage();
-
-  $scope.reversedMessage = function(message) {
-    return message.split("").reverse().join("");
-  };
-
-  $scope.fetchMessage = function(){
-    //调用工厂中getMessage方法来获取更新过的message
-
-    //调用控制域上reversedMessage方法来更新倒序显示的消息
-
-  };
-
-}

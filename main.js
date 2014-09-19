8ac47131cc41f821cbe601fc4c6a1a5da4e737dc
Ming He <meticulous.dft@gmail.com>
2014-09-19 01:05:31 -0700

    chapter 4 exercise 3 ng-transclusion

--- main.js
+++ main.js
@@ -1,86 +1,65 @@
 var myApp = angular.module('myApp', []);
 
-myApp.directive('button', function() {
+myApp.directive('alert', function () {
   return {
-    restrict: 'E',
-    //习题： 默认的按钮控件就有class="btn btn-primary"
-    //习题： attributes.type 可以为default,info,success,warning,danger,primary,根据type用css修饰按钮
-    compile: function(element, attributes) {
-      element.addClass('btn');
-      if ( attributes.type === 'submit' ) {
-        element.addClass('btn-primary');
-      }
-      if ( attributes.size ) {
-        element.addClass('btn-' + attributes.size);
-      }
+    restrict:'EA',
+    replace: true,
+    template:
+    '<div class="alert alert-{{type}}">' +
+      '<button type="button" class="close" ng-click="close()">&times;</button>' +
+      '<div ng-transclude></div>' +
+    '</div>',
+    transclude:true,
+    scope:{
+      type:'=',
+      close:'&'
     }
   };
 });
 
-myApp.directive('pagination', function() {
+myApp.directive('panel', function(){
   return {
-    restrict: 'E',
-    
-    scope: {
-      numPages: '=',
-      currentPage: '=',
-      onSelectPage: '&'
-    },
-
-    //习题：previous 改为 &laquo; 以及 next 改为 &raquo;
-    template:
-      '<div class="col-xs-offset-2"><ul class="pagination">' +
-        '<li ng-class="{disabled: noPrevious()}"><a ng-click="selectPrevious()">previous</a></li>' +
-        '<li ng-repeat="page in pages" ng-class="{active: isActive(page)}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
-        '<li ng-class="{disabled: noNext()}"><a ng-click="selectNext()">next</a></li>' +
-        '</ul>' +
-      '</div>',
-
+    restrict:'EA',
     replace: true,
-
-    //习题：增加跳到首页以及跳到尾页的方法
-    link: function(scope) {
-
-      scope.$watch('numPages', function(value) {
-        scope.pages = [];
-        for(var i=1;i<=value;i++) {
-          scope.pages.push(i);
-        }
-        if ( scope.currentPage > value ) {
-          scope.selectPage(value);
-        }
-      });
-
-      scope.noPrevious = function() {
-        return scope.currentPage === 1;
-      };
-
-      scope.noNext = function() {
-        return scope.currentPage === scope.numPages;
-      };
-
-      scope.isActive = function(page) {
-        return scope.currentPage === page;
-      };
-
-      scope.selectPage = function(page) {
-        if ( ! scope.isActive(page) ) {
-          scope.currentPage = page;
-          scope.onSelectPage({ page: page });
-        }
-      };
-
-      scope.selectPrevious = function() {
-        if ( !scope.noPrevious() ) {
-          scope.selectPage(scope.currentPage-1);
-        }
-      };
-
-      scope.selectNext = function() {
-        if ( !scope.noNext() ) {
-          scope.selectPage(scope.currentPage+1);
-        }
-      };
+    template:
+    '<div class="panel panel-{{type}}">' +
+      '<div class="panel-heading">{{title}}</div>' +
+      '<div class="panel-body"><div ng-transclude></div></div>' +
+    '</div>',
+    transclude:true,
+    scope:{
+      type:'=',
+      title: '=',
     }
+  }
+});
+
+myApp.controller('AlertController', function ($scope) {
+  $scope.alerts = [
+    { type: 'danger', msg: '系统遇到错误，以下是详细的错误信息。。。' }, 
+    { type: 'success', msg: '成功更新了您的资料' },
+    { type: 'info', msg: 'info 这里是详细的填写说明。。。'},
+    { type: 'warning', msg: '您的资料不完整，请仔细检查填写结果。'}
+  ];
+
+  $scope.addAlert = function() {
+    var newAlert = angular.copy($scope.alerts[Math.floor(Math.random()*$scope.alerts.length)]);
+    $scope.alerts.push(newAlert);
   };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
+  var panels = [
+    { type: 'danger', title: '错误', content: '系统遇到错误，以下是详细的错误信息。。。' }, 
+    { type: 'success', title: '成功', content: '成功更新了您的资料' },
+    { type: 'info', title: '信息', content: 'info 这里是详细的填写说明。。。'},
+    { type: 'warning', title: '警告' , content: '您的资料不完整，请仔细检查填写结果。'}
+  ];
+
+  $scope.randomPanel = function(){
+    $scope.panel = panels[Math.floor(Math.random()*panels.length)];
+  }
+
 });

aec73389e8f3b80283e781875be040ac43e06a5d
Ming He <hmchem@gmail.com>
2014-09-18 18:25:09 -0700

    chapter 4 exercise 1 directive intro

--- main.js
+++ main.js
@@ -1,50 +1,8 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('Avengers', function() {
-    var Avengers = {};
-    Avengers.cast = [
-      {
-        name: "Robert Downey Jr.",
-        character: "Tony Stark / Iron Man"
-      },
-      {
-        name: "Chris Evans",
-        character: "Steve Rogers / Captain America"
-      },
-      {
-        name: "Mark Ruffalo",
-        character: "Bruce Banner / The Hulk"
-      },
-      {
-        name: "Chris Hemsworth",
-        character: "Thor"
-      },
-      {
-        name: "Scarlett Johansson",
-        character: "Natasha Romanoff / Black Widow"
-      },
-      {
-        name: "Jeremy Renner",
-        character: "Clint Barton / Hawkeye"
-      },
-      {
-        name: "Tom Hiddleston",
-        character: "Loki"
-      }
-    ];
-    return Avengers;
-})
-
-function SuperherosCtrl($scope, Avengers) {
-    $scope.avengers = Avengers;
-    $scope.limit = 10;
-    $scope.reverse = false;
-
-    $scope.boxOfficeInUS = 220000000;
-    $scope.boxOfficeInCN = 1400000000;
-
-    $scope.rating = 8.234;
-
-    var releaseDate = new Date("2012-05-04");
-    $scope.release = releaseDate.getTime();
-}
+myApp.directive("helloworld", function(){
+  return {
+    restrict: 'E',
+    template: '<h2>指令(directive)是Angular中最强大的部分</h2>'
+  }
+});

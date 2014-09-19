8488c53ff70c0f033ae1a49ec3b6e7d541d47c0e
Ming He <meticulous.dft@gmail.com>
2014-09-19 01:41:23 -0700

    chapter 4 exercise 4 directive scope

--- main.js
+++ main.js
@@ -1,65 +1,29 @@
 var myApp = angular.module('myApp', []);
 
-myApp.directive('alert', function () {
-  return {
-    restrict:'EA',
-    replace: true,
-    template:
-    '<div class="alert alert-{{type}}">' +
-      '<button type="button" class="close" ng-click="close()">&times;</button>' +
-      '<div ng-transclude></div>' +
-    '</div>',
-    transclude:true,
-    scope:{
-      type:'=',
-      close:'&'
-    }
-  };
-});
-
-myApp.directive('panel', function(){
-  return {
-    restrict:'EA',
-    replace: true,
-    template:
-    '<div class="panel panel-{{type}}">' +
-      '<div class="panel-heading">{{title}}</div>' +
-      '<div class="panel-body"><div ng-transclude></div></div>' +
-    '</div>',
-    transclude:true,
-    scope:{
-      type:'=',
-      title: '=',
-    }
+myApp.controller("AppCtrl", function($scope) {
+  $scope.callContent = function(number, network, message) {
+    alert("号码： " + number + " 运营商： "+ network + " 通话内容： " + message);
   }
 });
 
-myApp.controller('AlertController', function ($scope) {
-  $scope.alerts = [
-    { type: 'danger', msg: '系统遇到错误，以下是详细的错误信息。。。' }, 
-    { type: 'success', msg: '成功更新了您的资料' },
-    { type: 'info', msg: 'info 这里是详细的填写说明。。。'},
-    { type: 'warning', msg: '您的资料不完整，请仔细检查填写结果。'}
-  ];
+myApp.directive("phone", function() {
+  return {
+    restrict: "E",
+    scope: {
+      number: "@",
+      network: "=",
+      makeCall: "&"
+    },
+    template: '<div class="panel panel-info"><div class="panel-heading">通话控制面板</div>'+
+      '<div class="panel-body"><p>呼叫号码: {{number}}</p>'+
+      '<p>我的运营商:<select ng-model="network" ng-options="net for net in networks"></select></p>' +
+      '<p><input type="text" ng-model="value" class="form-control" placeholder="输入通话内容"></p>' +
+      '<p><button class="btn btn-default" ng-click="makeCall({number: number, network:network, message: value})">拨打电话</button></p>'+
+      '</div></div>',
 
-  $scope.addAlert = function() {
-    var newAlert = angular.copy($scope.alerts[Math.floor(Math.random()*$scope.alerts.length)]);
-    $scope.alerts.push(newAlert);
-  };
-
-  $scope.closeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
-
-  var panels = [
-    { type: 'danger', title: '错误', content: '系统遇到错误，以下是详细的错误信息。。。' }, 
-    { type: 'success', title: '成功', content: '成功更新了您的资料' },
-    { type: 'info', title: '信息', content: 'info 这里是详细的填写说明。。。'},
-    { type: 'warning', title: '警告' , content: '您的资料不完整，请仔细检查填写结果。'}
-  ];
-
-  $scope.randomPanel = function(){
-    $scope.panel = panels[Math.floor(Math.random()*panels.length)];
+    link: function(scope) {
+      scope.networks = ["移动","联通"];
+      scope.network = scope.networks[0]
+    }
   }
-
 });

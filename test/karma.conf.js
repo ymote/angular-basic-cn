cfa125a8fd5522d4cf4a46c15a1af2f802e5d7c9
meticulous-dft <meticulous.dft@gmail.com>
2014-10-01 00:19:54 -0700

    mv node_modules to global & store bower components too

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,8 +4,8 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'lib/angular/angular.js',
+      'lib/angular-mocks/angular-mocks.js',
       '*.js',
       'test/unit/**/*.js'
     ],

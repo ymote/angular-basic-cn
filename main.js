9d8f170768d3b1ac0d01414a3e45acaf246a3ddf
meticulous-dft <meticulous.dft@gmail.com>
2014-10-07 15:00:11 -0700

    solution & e2e test

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ myApp.directive('button', function() {
     //习题： 默认的按钮控件就有class="btn btn-primary"
     //习题： attributes.type 可以为default,info,success,warning,danger,primary,根据type用css修饰按钮
     compile: function(element, attributes) {
-      element.addClass('btn');
+      element.addClass('btn btn-primary');
       if ( attributes.type === 'submit' ) {
         element.addClass('btn-primary');
       }
+      if ( attributes.type ) {
+        element.addClass('btn-'+attributes.type);
+      }      
       if ( attributes.size ) {
         element.addClass('btn-' + attributes.size);
       }

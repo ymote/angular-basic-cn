050771735415e128f8934070c0c6f633b0e99ce9
meticulous-dft <meticulous.dft@gmail.com>
2014-10-07 15:21:51 -0700

    add e2e test

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('Angular Directive', function() {
+
+  describe('index page', function() {
+
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+    it('should generate alert and panel widgets', function() {
+      var addAlertBtn = element(by.css('.add-alert'));
+      element.all(by.repeater('alert in alerts')).then(function(oldRows){
+        var oldLength = oldRows.length;
+        addAlertBtn.click();
+        element.all(by.repeater('alert in alerts')).then(function(newRows){
+          var newLength = newRows.length;
+          expect(newLength).toBe(oldLength+1);
+
+          var selectPanelBtn = element(by.css('.select-panel'));
+          selectPanelBtn.click();
+          var panel = element(by.css('.panel'));
+          var panelHead = element(by.css('.panel-heading'));
+          var panelBody = element(by.css('.panel-body'));
+          expect(panel.isPresent()).toBe(true);
+          expect(panelHead.isPresent()).toBe(true);
+          expect(panelBody.isPresent()).toBe(true);
+        });
+      });
+    });
+
+  });
+  
+});

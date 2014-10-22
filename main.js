7a5198a9c71be26ca225ca76ec3f6b736e8cf2ad
Meticulous DFT <meticulous.dft@gmail.com>
2014-10-22 14:33:23 -0700

    add hint about scope watch

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ myApp.controller("AppCtrl", function($scope,$http) {
   $scope.orderProp = 'age';
 
   //增加可以控制显示数量的函数
-  //显示的数量为$socpe.number,在页面上可以输入
+  //显示的数量为$socpe.number,在页面上可以输入, 提示：搜索$scope.$watch的用法
 
 
 

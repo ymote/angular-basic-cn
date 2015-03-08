93eb996595a229646e091d39b37f197abe93aa35
live4code <live4code@gmail.com>
2015-03-08 20:52:56 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,35 +2,33 @@
 
 describe('Test ng-repeat filter', function() {
 
-  describe('When I click the select button in a table row, the information '+
-    'should be displayed in the h3 tag', function() {
+  describe('select 4th table row', function() {
+    
+    var row, button, h3;
 
     beforeEach(function() {
       browser.get('index.html');
-    });
-
-    var checkRow = function(rowIndex){
-      var row = element(by.repeater('actor in avengers.cast').row(rowIndex));
-      var index = row.element(by.css('td:nth-child(1)')).getText();
-      var name = row.element(by.css('td:nth-child(2)')).getText();
-      var character = row.element(by.css('td:nth-child(3)')).getText();
-      var button = row.element(by.css('.btn-primary')); 
-      expect(index).toBe((rowIndex+1).toString());
-
+      row = element(by.repeater('actor in avengers.cast').row(3));
+      button = row.element(by.css('.btn-primary'));
+      h3 = element(by.css('h3'));
       button.click();
-      var h3 = element(by.css('h3'));
-      expect(h3.getText()).toContain(name);
-      expect(h3.getText()).toContain(character);
-    };
-    
-    it('select the first row and the 4th row should work', function() {
-      element(by.model('search')).clear();
-      checkRow(0);
-      checkRow(3);
     });
 
-
-
+    it('should show the index 4 in table row', function(){
+      var index = row.element(by.css('td:nth-child(1)')).getText();
+      expect(index).toBe((rowIndex+1).toString());
+    });
+    
+    it('should show actor name in h3 tag', function(){
+      var name = row.element(by.css('td:nth-child(2)')).getText();
+      expect(h3.getText()).toContain(name);
+    });
+    
+    it('should show character name in h3 tag', function(){
+      var character = row.element(by.css('td:nth-child(3)')).getText();
+      expect(h3.getText()).toContain(character);
+    });
+    
   });
   
 });

bfc90ba59bb4801fb533145ed3d4ba44a0811668
live4code <live4code@gmail.com>
2015-03-08 20:31:06 +0000

    change karma test message

--- test/unit/filterSpec.js
+++ test/unit/filterSpec.js
@@ -1,10 +1,11 @@
-describe('filter', function() {
+describe('Test Angular Filter', function() {
 
 	beforeEach(module('myApp'));
 
-	describe('shiftString', function() {
+	describe('shiftString filter should shift string by speficied number', function() {
 
-	  it('should shift string by specified number',
+	  it('shift \'abcdefg\' by 2 should return \'fgabcde\'; shift \'this is a filter by 3 \' should '+
+	    'return \'terthis is a fil\'',
 	      inject(function(shiftStringFilter) {
 	    expect(shiftStringFilter('abcdefg',2)).toBe('fgabcde');
 	    expect(shiftStringFilter('this is a filter',3)).toBe('terthis is a fil');

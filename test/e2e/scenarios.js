5de20c8566c8a645183ee8b164d90f505966562c
live4code <live4code@gmail.com>
2015-03-09 03:38:41 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,30 +1,29 @@
 'use strict';
 
-describe('Angular Pagination Directive', function() {
+describe('Test Pagination with 5 pages', function() {
 
-  describe('index page', function() {
+  describe('', function() {
 
     beforeEach(function() {
       browser.get('index.html');
-    });
-
-    it('should allow jump to first & last page', function() {
       var numPages = element(by.model('numPages'));
       numPages.clear();
-      numPages.sendKeys('5');      
-
-      var fisrtLink = element(by.css('.pagination')).element(by.css('li:first-child')).element(by.css('a'));
-      var fisrtLi = element(by.css('.pagination')).element(by.css('li:nth-child(2)'));
-      var lastLink = element(by.css('.pagination')).element(by.css('li:last-child')).element(by.css('a'));
-      var lastLi = element(by.css('.pagination')).element(by.css('li:nth-child(6)'));
-
-      fisrtLink.click();
-      expect(fisrtLi.getAttribute('class')).toContain('active');
-      lastLink.click();
-      expect(lastLi.getAttribute('class')).toContain('active');
-
+      numPages.sendKeys('5');       
     });
 
+    it('Click the left link in pagination should go to the first page -- the first <li> has class \'active\'', function(){
+      var fisrtLink = element(by.css('.pagination')).element(by.css('li:first-child')).element(by.css('a'));
+      var fisrtLi = element(by.css('.pagination')).element(by.css('li:nth-child(2)')); 
+      fisrtLink.click();
+      expect(fisrtLi.getAttribute('class')).toContain('active');
+    });
+
+    it('Click the right link in pagination should go to the last page -- the last <li> has class \'active\'', function(){
+      var lastLink = element(by.css('.pagination')).element(by.css('li:last-child')).element(by.css('a'));
+      var lastLi = element(by.css('.pagination')).element(by.css('li:nth-child(6)')); 
+      lastLink.click();
+      expect(lastLi.getAttribute('class')).toContain('active');
+    });    
   });
   
 });
--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,5 +1,11 @@
+var fs = require('fs');
+var re = /\S+/;
+
+var myIP = re.exec(fs.readFileSync('/etc/hosts','utf8'))[0];
+var driverIP = '172.17.42.1';
+
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://'+driverIP+':4444/wd/hub',
 
   allScriptsTimeout: 11000,
 
@@ -29,7 +35,7 @@ exports.config = {
 
   //chromeOnly: false,
 
-  baseUrl: 'http://localhost:8000/',
+  baseUrl: 'http://'+myIP+':8000/',
 
   framework: 'jasmine',
 

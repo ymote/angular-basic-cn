ce75d396da5b0c5bb090c4c7a7345e64817a7d9a
live4code <live4code@gmail.com>
2015-03-09 03:39:26 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Test Pagination with 5 pages', function() {
+describe('Test Pagination with 5 pages.', function() {
 
   describe('', function() {
 
@@ -11,14 +11,14 @@ describe('Test Pagination with 5 pages', function() {
       numPages.sendKeys('5');       
     });
 
-    it('Click the left link in pagination should go to the first page -- the first <li> has class \'active\'', function(){
+    it('Click the left link in pagination should go to the first page -- the first <li> has class \'active\'.', function(){
       var fisrtLink = element(by.css('.pagination')).element(by.css('li:first-child')).element(by.css('a'));
       var fisrtLi = element(by.css('.pagination')).element(by.css('li:nth-child(2)')); 
       fisrtLink.click();
       expect(fisrtLi.getAttribute('class')).toContain('active');
     });
 
-    it('Click the right link in pagination should go to the last page -- the last <li> has class \'active\'', function(){
+    it('Click the right link in pagination should go to the last page -- the last <li> has class \'active\'.', function(){
       var lastLink = element(by.css('.pagination')).element(by.css('li:last-child')).element(by.css('a'));
       var lastLi = element(by.css('.pagination')).element(by.css('li:nth-child(6)')); 
       lastLink.click();

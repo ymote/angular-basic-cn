7d2680bd14b076a7543fa4488350150dc76a99ea
live4code <live4code@gmail.com>
2015-03-09 03:50:53 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,14 +1,14 @@
 'use strict';
 
-describe('Angular Directive', function() {
+describe('Test Transclude.', function() {
 
-  describe('index page', function() {
+  describe('', function() {
 
     beforeEach(function() {
       browser.get('index.html');
     });
-
-    it('should generate alert and panel widgets', function() {
+  
+    it('Click the addAlert button should create a new alert widget from the 4 alerts.', function(){
       var addAlertBtn = element(by.css('.add-alert'));
       element.all(by.repeater('alert in alerts')).then(function(oldRows){
         var oldLength = oldRows.length;
@@ -16,19 +16,22 @@ describe('Angular Directive', function() {
         element.all(by.repeater('alert in alerts')).then(function(newRows){
           var newLength = newRows.length;
           expect(newLength).toBe(oldLength+1);
-
-          var selectPanelBtn = element(by.css('.select-panel'));
-          selectPanelBtn.click();
-          var panel = element(by.css('.panel'));
-          var panelHead = element(by.css('.panel-heading'));
-          var panelBody = element(by.css('.panel-body'));
-          var panelContent = element(by.css('.panel-body div span'));
-          expect(panel.isPresent()).toBe(true);
-          expect(panelHead.isPresent()).toBe(true);
-          expect(panelContent.getText()).toBeTruthy();
-          expect(panelBody.isPresent()).toBe(true);
         });
-      });
+      });      
+    });
+    
+    it('Click the randomPanel button should create a new panel from panels. All parts of panel -- '
+      +'panel-heading, panel-body and panel content should present.', function(){
+      var selectPanelBtn = element(by.css('.select-panel'));
+      selectPanelBtn.click();
+      var panel = element(by.css('.panel'));
+      var panelHead = element(by.css('.panel-heading'));
+      var panelBody = element(by.css('.panel-body'));
+      var panelContent = element(by.css('.panel-body div span'));
+      expect(panel.isPresent()).toBe(true);
+      expect(panelHead.isPresent()).toBe(true);
+      expect(panelContent.getText()).toBeTruthy();
+      expect(panelBody.isPresent()).toBe(true);    
     });
 
   });
